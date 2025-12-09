19d1c6885bd8b1dd676b47c81b81a42b1f0570eb
mackaronina <mihamishu123@gmail.com>
2025-12-10 01:41:38 +0200

    Used pyaudio to play sound

---
+++ utils/audio_player.py
@@ -0,0 +1,25 @@
+import wave
+from io import BytesIO
+
+import pyaudio
+
+from config import AUDIO_CHUNK_SIZE
+
+
+class AudioPlayer:
+    def __init__(self):
+        self.player = pyaudio.PyAudio()
+
+    def play_wav_brom_binary(self, audio: BytesIO) -> None:
+        with wave.open(audio, 'rb') as wf:
+            stream = self.player.open(format=self.player.get_format_from_width(wf.getsampwidth()),
+                                      channels=wf.getnchannels(),
+                                      rate=wf.getframerate(),
+                                      output=True
+                                      )
+            while len(data := wf.readframes(AUDIO_CHUNK_SIZE)):  # Requires Python 3.8+ for :=
+                stream.write(data)
+            stream.close()
+
+    def quit(self):
+        self.player.terminate()
